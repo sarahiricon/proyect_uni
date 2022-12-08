906e5eb77336587c10f8eee73b0a7e8c850b33ad
sarahiricon <119866605+sarahiricon@users.noreply.github.com>
2022-12-07 22:05:40 -0600

    Add files via upload

---
+++ 19070695_03.js
@@ -0,0 +1,20 @@
+const str=' ,se, fuerte, para, que, nadien, te, lastime, se, hulmide, para, que, nadien, te, humille';
+console.log("frase comas");
+console.log("frase: ",str)
+const words=str.split(',');
+
+console.log(words[1]);
+console.log(words[2]);
+console.log(words[3]);
+console.log(words[4]);
+console.log(words[5]);
+console.log(words[6]);
+console.log(words[7]);
+console.log(words[8]);
+console.log(words[9]);
+console.log(words[10]);
+console.log(words[11]);
+console.log(words[12]);
+console.log(words[13]);
+console.log(words[14]);
+console.log(words[15]);
