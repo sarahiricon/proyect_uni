4b00d877fff586f99f2614a79d12934399a4119a
sarahiricon <119866605+sarahiricon@users.noreply.github.com>
2022-12-07 21:32:58 -0600

    Add files via upload

---
+++ 19070695_01.js
@@ -0,0 +1,8 @@
+let nombrecompleto='          Ivonne Sarahi Rico Nájera...........xD          '
+
+console.log("trim");
+console.log(nombrecompleto.trim());
+console.log("trim start");
+console.log(nombrecompleto.trimStart());
+console.log("trim end:");
+console.log(nombrecompleto.trimEnd());
